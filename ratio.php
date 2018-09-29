f1adad1ae4d7bc56e14887d21efdc58b2ebc0339
Datagit <datagit@yahoo.com>
2018-09-30 00:00:02 +0700

    Ratio2

--- ratio.php
+++ ratio.php
@@ -5,9 +5,10 @@
  * Date: 9/29/18
  * Time: 4:41 PM
  */
-$re = '/(?<index>\d+):(?<percentage>\d+)/m';
+$re = '/(?<sheet_id>\d+).(?<position>\d+):(?<percent>\d+)/m';
+$str = '1.1:110,1.2:150';
 //$str = '1:80,2:20';
-$str = '1:80';
+//$str = '1:80';
 
 preg_match_all($re, $str, $matches, PREG_SET_ORDER, 0);
 
@@ -15,49 +16,44 @@ if (empty($matches)) {
     return false;
 }
 $templates = array();
-$sum_percentage = 0;
+$sum_percent = 0;
+$positions = array();
 foreach ($matches as $values) {
-    $sum_percentage += $values['percentage'];
+    $values['percent'] = $values['percent'] % 100;
+    $sum_percent += $values['percent'];
     $templates[] = array(
-        'index' => $values['index'],
-        'percentage' => $values['percentage'],
+        'sheet_id' => $values['sheet_id'],
+        'position' => $values['position'],
+        'percent' => $values['percent'],
     );
+    $positions[] = $values['position'];
 }
 print_r($templates);
-
-if ($sum_percentage > 100) {
-    return false;
-}
 
 $key_value = '';
 $sum_ratio = 0;
 $random_ratio = mt_rand(1,100);
-echo "sum_percentage = $sum_percentage\n";
+echo "sum_percent = $sum_percent\n";
 echo "random_ratio = $random_ratio\n";
-if ($sum_percentage == 100) {
-    //$str = '1:80,2:20';
-    foreach ($templates as $template) {
-        $sum_ratio += $template['percentage'];
-        if ($random_ratio <= $sum_ratio) {
-            $key_value = $template['index'];
-            break;
-        }
-    }
-} else {
-
-    //$str = '1:50,2:20';
-    //$sum_percentage == 70
-    foreach ($templates as $template) {
-        $sum_ratio += $template['percentage'];
-        if ($random_ratio <= $sum_ratio) {
-            $key_value = $template['index'];
-            break;
-        }
-    }
-    if ($random_ratio > $sum_percentage) {
-        //3,4,5,6,7,8,9,10
-        $key_value = mt_rand(3,10);
+foreach ($templates as $template) {
+    $sum_ratio += $template['percent'];
+    if ($random_ratio <= $sum_ratio) {
+        $key_value = $template['position'];
+        break;
     }
 }
-var_dump($key_value);
-echo "pick from random $key_value\n";
+if (!empty($key_value)) {
+    echo "1. pick from random $key_value\n";
+}
+
+if (empty($key_value)) {
+    if ($random_ratio > $sum_percent) {
+        //3,4,5,6,7,8,9,10
+        $default_positions = range(1,10);
+        $positions = array_diff($default_positions, $positions);
+        $positions = array_values($positions);
+        $key_value = $positions[array_rand($positions)];
+    }
+
+    echo "2. pick from random $key_value\n";
+}
