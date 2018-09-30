5c4d50cb8684e361b0175175e35ad760dde6cc9d
Datagit <datagit@yahoo.com>
2018-09-30 16:42:07 +0700

    ratio perty code

--- ratio.php
+++ ratio.php
@@ -5,53 +5,76 @@
  * Date: 9/29/18
  * Time: 4:41 PM
  */
-$re = '/(?<sheet_id>\d+).(?<position>\d+):(?<percent>\d+)/m';
-$str = '1.1:110,1.2:150';
-//$str = '1:80,2:20';
-//$str = '1:80';
 
-preg_match_all($re, $str, $matches, PREG_SET_ORDER, 0);
+main();
 
-if (empty($matches)) {
-    return false;
-}
-$templates = array();
-$sum_percent = 0;
-$positions = array();
-foreach ($matches as $values) {
-    $values['percent'] = $values['percent'] % 100;
-    $sum_percent += $values['percent'];
-    $templates[] = array(
-        'sheet_id' => $values['sheet_id'],
-        'position' => $values['position'],
-        'percent' => $values['percent'],
-    );
-    $positions[] = $values['position'];
-}
-print_r($templates);
-
-$key_value = '';
-$sum_ratio = 0;
-$random_ratio = mt_rand(1,100);
-echo "sum_percent = $sum_percent\n";
-echo "random_ratio = $random_ratio\n";
-foreach ($templates as $template) {
-    $sum_ratio += $template['percent'];
-    if ($random_ratio <= $sum_ratio) {
-        $key_value = $template['position'];
-        break;
+function main() {
+    $pattern = '/(?<sheet_id>\d+).(?<position>\d+):(?<percent>\d+)/m';
+    $str = '1.1:10,1.2:50';
+    //$str = '1:80,2:20';
+    //$str = '1:80';
+    $result = parseInput($pattern, $str);
+    if (empty($result)) {
+        return;
     }
-}
-if (!empty($key_value)) {
-    echo "1. pick from random $key_value\n";
+    list($data_user_input, $positions) = $result;
+    $random_ratio = mt_rand(1,100);
+    $value = pickByPercent($random_ratio, $data_user_input);
+    if (empty($value)) {
+        $default = generateDefault();
+        $positions = array_diff($default, $positions);
+        if (!empty($positions)) {
+            $value = pickByArray($positions);
+        }
+    }
+    return $value;
 }
 
-if (empty($key_value)) {
-    //3,4,5,6,7,8,9,10
-    $default_positions = range(1,10);
-    $positions = array_diff($default_positions, $positions);
+function parseInput($pattern, $str) {
+    preg_match_all($pattern, $str, $matches, PREG_SET_ORDER, 0);
+    if (empty($matches)) {
+        return false;
+    }
+    $data_user_input = array();
+    $sum_percent = 0;
+    $positions = array();
+    foreach ($matches as $values) {
+        $values['percent'] = abs($values['percent']);
+        $values['percent'] = ($values['percent'] >= 100) ? 100 : $values['percent'] % 100;
+        $sum_percent += $values['percent'];
+        $data_user_input[] = array(
+            'sheet_id' => $values['sheet_id'],
+            'position' => $values['position'],
+            'percent' => $values['percent'],
+        );
+        $positions[] = $values['position'];
+    }
+    return array($data_user_input, $positions);
+}
+
+function pickByPercent($random_ratio, $percents) {
+    $sum_ratio = 0;
+    $position = '';
+    echo "random ratio $random_ratio\n";
+    foreach ($percents as $template) {
+        $sum_ratio += $template['percent'];
+        if ($random_ratio <= $sum_ratio) {
+            $position = $template['position'];
+            echo "1. pick from random $position\n";
+            break;
+        }
+    }
+    echo "sum ratio $sum_ratio\n";
+    return $position;
+}
+
+function pickByArray($positions) {
     $positions = array_values($positions);
-    $key_value = $positions[array_rand($positions)];
+    $position = $positions[array_rand($positions)];
+    echo "2. pick from random $position\n";
+    return $position;
+}
 
-    echo "2. pick from random $key_value\n";
+function generateDefault() {
+    return range(1,10);
 }
